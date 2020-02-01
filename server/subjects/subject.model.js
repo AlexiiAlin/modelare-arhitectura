454b4cd324093e5a419e590514a966f220c7f7c4
EINazare <emanuelioannazare@gmail.com>
2020-02-01 20:36:27 +0200

    style(): run prettier command

--- server/subjects/subject.model.js
+++ server/subjects/subject.model.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    name: { type: String, required: true },
-    classSubjects: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'ClassSubject'
-    }]
+  name: { type: String, required: true },
+  classSubjects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ClassSubject"
+    }
+  ]
 });
 
-schema.set('toJSON', { virtuals: true });
+schema.set("toJSON", { virtuals: true });
 
-module.exports = mongoose.model('Subject', schema);
+module.exports = mongoose.model("Subject", schema);
