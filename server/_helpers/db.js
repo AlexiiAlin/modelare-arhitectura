c92e179b1ab9c421c38d3615f7d8282089c13d63
EINazare <emanuelioannazare@gmail.com>
2020-02-01 20:35:49 +0200

    fix(): add useUnifiedTopology: true

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -1,12 +1,16 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+const config = require("config.json");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model'),
-    Class: require('../classes/classes.model'),
-    ClassSubject: require('../classSubject/classSubject.model'),
-    Subject: require('../subjects/subject.model'),
-    Grade: require('../grades/grade.model'),
+  User: require("../users/user.model"),
+  Class: require("../classes/classes.model"),
+  ClassSubject: require("../classSubject/classSubject.model"),
+  Subject: require("../subjects/subject.model"),
+  Grade: require("../grades/grade.model")
 };
