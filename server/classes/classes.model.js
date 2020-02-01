dcc91e8fefdfa42e6fc56767fe4e8e924b3af306
EINazare <emanuelioannazare@gmail.com>
2020-02-01 20:36:09 +0200

    style(): run prettier command

--- server/classes/classes.model.js
+++ server/classes/classes.model.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    name: { type: String, unique: true, required: true },
-    students: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    teachers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    classSubjects: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'ClassSubject'
-    }]
+  name: { type: String, unique: true, required: true },
+  students: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  teachers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  classSubjects: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ClassSubject"
+    }
+  ]
 });
 
-schema.set('toJSON', { virtuals: true });
+schema.set("toJSON", { virtuals: true });
 
-module.exports = mongoose.model('Class', schema);
+module.exports = mongoose.model("Class", schema);
