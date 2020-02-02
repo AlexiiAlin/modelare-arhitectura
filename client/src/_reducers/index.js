06be110cd0396f54343d4986f535c01bb6bae869
EINazare <emanuelioannazare@gmail.com>
2020-02-02 16:09:30 +0200

    feature(): add get all classes route for client

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import { authentication } from "./authentication.reducer";
 import { users } from "./users.reducer";
 import { alert } from "./alert.reducer";
+import { classes } from "./classes.reducer";
 
 const rootReducer = combineReducers({
   authentication,
   users,
-  alert
+  alert,
+  classes
 });
 
 export default rootReducer;
