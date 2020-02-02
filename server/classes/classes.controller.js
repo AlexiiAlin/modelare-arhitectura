56bb542ff5fe360379fb7719a0aa9d6cbbc8dfa0
EINazare <emanuelioannazare@gmail.com>
2020-02-02 17:40:59 +0200

    feature(): add class creation

--- server/classes/classes.controller.js
+++ server/classes/classes.controller.js
@@ -4,11 +4,19 @@ const classService = require("./class.service");
 
 // routes
 router.get("/", getAllClasses);
+router.post("/", createClass);
 
 function getAllClasses(req, res, next) {
   classService
     .getAllClasses()
     .then(classes => res.json(classes))
+    .catch(err => next(err));
+}
+
+function createClass(req, res, next) {
+  classService
+    .createClass(req.body)
+    .then(classObj => res.json(classObj))
     .catch(err => next(err));
 }
 
