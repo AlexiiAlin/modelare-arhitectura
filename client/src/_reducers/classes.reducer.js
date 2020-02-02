ae58090575646714e75f42138d4bfd63f25d1f62
EINazare <emanuelioannazare@gmail.com>
2020-02-02 16:09:27 +0200

    feature(): add get all classes route for client

---
+++ client/src/_reducers/classes.reducer.js
@@ -0,0 +1,21 @@
+import { classConstants } from "../_constants";
+
+export function classes(state = {}, action) {
+  switch (action.type) {
+    // START TEACHER
+    case classConstants.GETALL_CLASSES_REQUEST:
+      return {
+        loading: true
+      };
+    case classConstants.GETALL_CLASSES_SUCCESS:
+      return {
+        items: action.classes
+      };
+    case classConstants.GETALL_CLASSES_FAILURE:
+      return {
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
