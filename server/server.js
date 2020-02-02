6300546f03d436ade2e9c4d7c0997aa2338f0781
EINazare <emanuelioannazare@gmail.com>
2020-02-02 17:10:46 +0200

    refactor(): change the db initializer

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const errorHandler = require("_helpers/error-handler");
 const mongoose = require("mongoose");
 const config = require("config.json");
 const db = mongoose.createConnection(config.connectionString);
-const dbEntities = require("_helpers/db");
+const initializeDb = require("_helpers/initializeDb");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,74 +33,5 @@ const port =
   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
 const server = app.listen(port, function() {
   console.log("Server listening on port " + port);
-  //initializeDb();
+  initializeDb(db);
 });
-
-function initializeDb() {
-  console.log("Initializing db");
-  const { User, Class, ClassSubject, Grade, Subject } = dbEntities;
-  const user = new User({
-    username: "Test" + Date.now(),
-    hash: "test",
-    firstName: "test",
-    lastName: "test"
-  });
-  const classObj = new Class({
-    name: "402 " + Date.now()
-  });
-  const subject = new Subject({
-    name: "Math"
-  });
-  const grade = new Grade({});
-
-  console.log("Initializing db : Connecting to db...");
-  db.once("connected", function(err) {
-    console.log("Initializing db : Connected to db!");
-    console.log("Initializing db : Add User");
-    if (err) {
-      return console.error(err);
-    }
-    User.create(user, function(err, doc) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("Initializing db : User successfully added!");
-    });
-
-    console.log("Initializing db : Add Class");
-    Class.create(classObj, function(err, doc) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("Initializing db : Class successfully added!");
-    });
-
-    console.log("Initializing db : Add Subject");
-    Subject.create(subject, function(err, doc) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("Initializing db : Subject successfully added!");
-    });
-
-    const classSubject = new ClassSubject({
-      class: classObj,
-      subject
-    });
-    console.log("Initializing db : Add ClassSubject");
-    ClassSubject.create(classSubject, function(err, doc) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("Initializing db : ClassSubject successfully added!");
-    });
-
-    console.log("Initializing db : Add Grade");
-    Grade.create(grade, function(err, doc) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log("Initializing db : Grade successfully added!");
-    });
-  });
-}
