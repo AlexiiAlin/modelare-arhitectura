c171b7f33b6cb874b493e5bcc7d70222bbec907f
EINazare <emanuelioannazare@gmail.com>
2020-02-02 18:51:54 +0200

    refactor(): make new layout for class object

--- client/src/views/admin-layout/Class.js
+++ client/src/views/admin-layout/Class.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { connect } from "react-redux";
 // reactstrap components
 import {
+  Alert,
   Button,
   Card,
   CardHeader,
   CardBody,
   FormGroup,
   Input,
+  Form,
   Container,
   Row,
   Col,
@@ -16,85 +18,89 @@ import {
 // core components
 import Header from "components/Headers/Header.js";
 
-class Teacher extends React.Component {
+import { classActions, alertActions } from "_actions";
+
+class Class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        email: undefined
+      },
+      submitted: false
+    };
+  }
+  componentDidMount() {
+    let pathName = window.location.href.split("/");
+    let classId = pathName[pathName.length - 1];
+    if (/\d/.test(classId)) {
+    } else {
+      window.location.reload();
+    }
+    // this.props.getClass(classId);
+  }
+  componentWillUnmount() {
+    this.props.clearAlerts();
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    const { user } = this.state;
+    this.setState({
+      user: {
+        ...user,
+        [name]: value
+      }
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ submitted: true });
+    const { user } = this.state;
+    this.setState({ user: newUser });
+    if (user.email) {
+      this.props.addNewStudent(user);
+    }
+  };
   render() {
+    const { alert, students, classObj } = this.props;
+    const { user, submitted } = this.state;
     return (
       <>
         <Header />
         {/* Page content */}
         <Container className="mt--7" fluid>
+          {alert.message ? (
+            <Alert color={alert.type}>{alert.message}</Alert>
+          ) : null}
           <Row>
             <Col md="6">
               <Card className="shadow">
                 <CardHeader className="border-0">
-                  <h3 className="mb-0">8th students</h3>
+                  <h3 className="mb-0">{classObj && classObj.name} students</h3>
                 </CardHeader>
-                <Table className="align-items-center table-flush" responsive>
-                  <thead className="thead-light">
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Student</th>
-                      <th scope="col" className="text-right grades-actions">
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Johan Fullman</td>
-                      <td className="text-right">
-                        <Button color="danger" size="sm">
-                          Delete
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Jane Dane</td>
-                      <td className="text-right">
-                        <Button color="danger" size="sm">
-                          Delete
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Jon Doe</td>
-                      <td className="text-right">
-                        <Button color="danger" size="sm">
-                          Delete
-                        </Button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </Table>
+                <Table students={students || []} />
                 <CardBody className="bg-secondary">
-                  <Row>
-                    <Col md="6">
-                      <FormGroup>
-                        <Input
-                          className="form-control-alternative"
-                          id="input-first-name"
-                          placeholder="First Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col md="6">
-                      <FormGroup>
-                        <Input
-                          className="form-control-alternative"
-                          id="input-last-name"
-                          placeholder="Last Name"
-                          type="text"
-                        />
-                      </FormGroup>
-                    </Col>
-                    <Col md="12">
-                      <Button color="primary">Add new Student</Button>
-                    </Col>
-                  </Row>
+                  <Form onSubmit={this.handleSubmit}>
+                    <Row>
+                      <Col md="8">
+                        <FormGroup>
+                          <Input
+                            className="form-control-alternative"
+                            id="input-last-name"
+                            placeholder="Email of student"
+                            type="email"
+                            name="email"
+                            onChange={this.onChange}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md="4">
+                        <Button color="primary">Add new Student</Button>
+                      </Col>
+                    </Row>
+                  </Form>
                 </CardBody>
               </Card>
             </Col>
@@ -105,4 +111,20 @@ class Teacher extends React.Component {
   }
 }
 
-export default Teacher;
+function mapState(state) {
+  const { alert } = state;
+  const { students, classObj } = state.classes;
+  return { alert };
+}
+
+const actionCreators = {
+  getClass: classActions.getClass,
+  addNewStudent: classActions.addNewStudent,
+  clearAlerts: alertActions.clear
+};
+
+const connectedClass = connect(
+  mapState,
+  actionCreators
+)(Class);
+export default connectedClass;
